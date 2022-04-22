d0395a97a985c1b258c9d3d290cf6a601dec6c43
FillinGood <fargust97@gmail.com>
2022-04-23 05:41:01 +0700

    Beautification

--- index.js
+++ index.js
@@ -1,67 +1,117 @@
 // @ts-check
-(() => {
+(async () => {
   /**
    * List of pages to display on main page.
    * @type {string[]}
    */
   const pages = ["TrelLike"];
 
+  const ghCache = await fetch(
+    "https://api.github.com/search/repositories?q=user:FillinGood"
+  )
+    .then((x) => x.json())
+    .then((x) =>
+      x.items.reduce((a, x) => ({ ...a, [x.name]: x.description ?? "" }), {})
+    );
+  console.debug(ghCache);
+
   /**
    * Create a new HTML element
    * @param {K} name
-   * @param {({children:(HTMLElement|string)[]} & Record<string, any>)} [props]
+   * @param {Partial<HTMLElementTagNameMap[K]> | null} props
+   * @param {...(Node | string)} children
    * @template {keyof HTMLElementTagNameMap} K
    * @return {HTMLElementTagNameMap[K]}
    */
-  function el(name, props) {
+  function el(name, props, ...children) {
     const e = document.createElement(name);
     if (props) {
       for (const key in props) {
-        if (key === "children") {
-          for (const child of props.children) {
-            if (typeof child === "string")
-              e.appendChild(document.createTextNode(child));
-            else e.appendChild(child);
-          }
-        } else {
-          e[key] = props[key];
-        }
+        e[key] = props[key];
+      }
+    }
+    if (children) {
+      for (const child of children) {
+        if (typeof child === "string")
+          e.appendChild(document.createTextNode(child));
+        else e.appendChild(child);
       }
     }
     return e;
   }
+  function sleep(ms) {
+    return new Promise((res) => setTimeout(() => res(), ms));
+  }
 
-  function onload() {
-    const table = document.getElementById("pages");
-    pages
-      .map((x) =>
-        el("tr", {
-          children: [
-            el("td", {
-              children: [
-                el("a", {
-                  children: [x],
-                  href: "/" + x,
-                }),
-              ],
-            }),
-            el("td", {
-              children: [
-                "(",
-                el("a", {
-                  children: ["git"],
-                  href: ["https://github.com/FillinGood/" + x],
-                }),
-                ")",
-              ],
-            }),
-          ],
-        })
-      )
-      .forEach((x) => table.appendChild(x));
+  /**
+   * Get GH repository short description
+   * @param {string} repo
+   * @return {string|null}
+   */
+  function getDescription(repo) {
+    return ghCache[repo] ?? null;
+  }
+  /**
+   * @param {string} text
+   * @return {Element}
+   */
+  function elFromString(text) {
+    const tpl = el("template", { innerHTML: text.trim() });
+    return tpl.content.firstElementChild;
+  }
+
+  async function onload() {
+    const app = document.getElementById("app");
+    const ghText = await fetch("/github.svg").then((x) => x.text());
+
+    let height = app.getBoundingClientRect().height;
+    app.style.height = height + "px";
+    app.classList.add("animate");
+
+    /** @type {Record<'name'|'app'|'repo'|'description', string>[]} */
+    const repos = pages.map((x) => ({
+      name: x,
+      app: "/" + x,
+      repo: "https://github.com/FillinGood/" + x,
+      description: getDescription(x),
+    }));
+    console.debug(repos);
+
+    for (const repo of repos) {
+      const item = el(
+        "div",
+        { className: "repo new-item" },
+        el(
+          "a",
+          { className: "name", href: repo.app },
+          el("span", null, repo.name),
+          el("a", { href: repo.repo }, elFromString(ghText))
+        ),
+        el(
+          "div",
+          { className: "description" },
+          repo.description ||
+            el(
+              "span",
+              { className: "error" },
+              repo.description === null ? "Not found" : "No description"
+            )
+        )
+      );
+      app.appendChild(item);
+      await sleep(400);
+      item.classList.remove("new-item");
+      height = item.getBoundingClientRect().bottom;
+      app.style.height = height + "px";
+    }
+    await sleep(400);
+    app.classList.add("end");
+    height += 5;
+    app.style.height = height + "px";
   }
 
   document.onreadystatechange = function () {
-    if (document.readyState === "interactive") onload();
+    if (document.readyState === "complete") onload();
   };
+  if (document.readyState === "complete") onload();
 })();
